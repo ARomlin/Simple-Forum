e5f2a4e2fbfa725b420660a8998c04598e73ccbb
AndersGn <graneman@yahoo.se>
2016-01-21 19:27:18 +0100

    added static so that HTML files are shown

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Simple-Forum
 Simple API uppgift (School assignment).
+
+Need to have mongoDB (default port) on localhost installed.
--- index.js
+++ index.js
@@ -1,89 +1,22 @@
+// Dependencies
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    restful = require('node-restful'), // Create awesome APIs using express
+    mongoose = restful.mongoose;
 
-var express = require('express');
+// MongoDB
+mongoose.connect('mongodb://localhost/threads'); // Connect to local database
+
+// Express
 var app = express();
 
+// Default settings
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
-var threads = [
-    {id: 1, title: 'En titel här', text: 'Lite text här!'},
-    {id: 2, title: 'En titel här2', text: 'Lite text här2!'},
-    {id: 3, title: 'En titel här3', text: 'Lite text här3!'},
-    {id: 4, title: 'En titel här4', text: 'Lite text här4!'},
-    {id: 5, title: 'En titel här5', text: 'Lite text här5!'}]
-
-//GET
-
-app.get('/threads', function (req, res) {
-   res.json( {threads: threads});
-});
-
-//GET id
-
-app.get('/threads/:id', function (req, res) {
-    var thread = threads[req.params.id];
-   res.json(thread);
-    if(req.params.id === '666') {
-
-        console.log('The beast welcomes you! Moahahaha!');
-    } else {
-        console.log('id is: ' + req.params.id);
-    }
-});
-
-
-
-
-app.put('/threads/:id', function (req, res) {
-    var id = req.params.id;
-    var status = 'ERR';
-
-    if (typeof threads[id] !== 'undefined') {
-
-        res.status(201);
-
-        threads[id].id = req.body.id;
-        threads[id].title = req.body.title;
-        threads[id].text = req.body.text;
-
-        status = 'OK';
-    } else {
-        res.status(400);
-    }
-    res.json({ status: status})
-});
-
-app.post('/threads', function (req, res) {
-
-if(!isNaN(req.body.id) === true) {
-    res.status(201);
-
-    threads.push({
-        id: Number(req.body.id),
-        title: req.body.title,
-        text: req.body.text
-    });
-    console.log(!isNaN(req.body.id));
-    res.json({ message: 'You posted something!', insertId: threads.length - 1 });
-} else {
-    res.status(400);
-    res.json({ message: 'Please insert a number as a id!'});
-}
-
-});
-
-app.delete('/threads/:id', function (req, res) {
-    delete threads[req.params.id];
-
-    res.json({ message: 'Thread deleted successful!' })
-});
-
+// Routes
+app.use('/', require('./lib/api'));
 
 app.use(express.static(__dirname + '/public'));
 
-// Start up server on port 3000
 app.listen(3000);
-console.log('Magic is on port 3000!');
