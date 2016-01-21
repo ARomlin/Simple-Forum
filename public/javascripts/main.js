6b46dc0e53ca1ef28612f3fd4681290fa87ebff4
Andreas Romlin <andreas.romlin@live.se>
2016-01-21 21:45:55 +0100

    Fixat post på klienten

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,30 +1,40 @@
-/**
- * Created by andreasromlin on 2016-01-21.
- */
+
 
 $.ajax({
     url: "/threads"
 }).done(function(data) {
     console.log('Fetched / threads: ', data);
 
-    for(var i=0; i< data.threads.length; i++){
-        console.log(data.threads[i].id + ': ' + data.threads[i].title + ': ' + data.threads[i].text);
-        $('#showThreads').append('<p>' + '<b>id: </b>' + '<p id="thread' + data.threads[i].id + '" class="linkThread">' + data.threads[i].id + '</p>' + ' <b>title:</b> ' + data.threads[i].title + '<br />' +' <b>text:</b> ' + data.threads[i].text + '</p><hr /><br />');
+    for(var i=0; i< data.length; i++){
+        console.log(data[i]._id + ': ' + data[i].title + ': ' + data[i].text);
+        $('#showThreads').append('<p>' + '<b>id: </b>' + '<p id="thread' + data[i]._id + '" class="linkThread">' + data[i]._id + '</p>' + ' <b>title:</b> ' + data[i].title + '<br />' +' <b>text:</b> ' + data[i].text + '</p><hr /><br />');
     }
 });
 
-$('#submitButton').on('click', function() {
-    var $id = $('#id2').val();
-    var $title = $('#title2').val();
-    var $text = $('#text2').val();
 
+$('#submitButton').on('click', function() {
+
+    var $title = $('#title2').val();
+    var $id = $.ajax({
+        url: "/threads"
+    }).done(function (data) {
+        console.log('Fetched / threads: ', data);
+        for (var i = data.length; i > 0; i -= data.length) {
+            $id = data[data.length - 1]._id;
+            console.log($id);
+            return $id;
+        }
+    });
+    var $text = $('#text2').val();
     $.ajax({
             method: "POST",
             url: "/threads",
-            data: { id: $id, title: $title, text: $text }
+            data: { title: $title, text: $text }
         })
         .done(function( ) {
-            console.log( "Data Saved: " + 'id: ' + $id + ' : ' + 'title: ' + $title + ' : ' + 'text: ' +$text);
-            $('#showThreads').append('<p>' + '<b>id: </b>' + '<p id="thread' + $id + '" class="linkThread">' + $id + '</p>' + ' <b>title:</b> ' + $title + '<br />' +' <b>text:</b> ' + $text + '</p><hr /><br />');
+
+            console.log( "Data Saved: " + 'title: ' + $title + ' : ' + 'text: ' +$text);
+            $('#showThreads').append('<p>' + ' <b>id:</b> ' + '<p class="linkThread" id="thread' + $id + '">' + $id + '</p>' + '<br />' + ' <b>title:</b> ' + $title + '<br />' +' <b>text:</b> ' + $text + '</p><hr /><br />');
         });
+
 });
