24df10a78149dcd2cc54c275dbe5b5ee42dc88d8
Andreas Romlin <andreas.romlin@live.se>
2016-01-22 20:22:54 +0100

    Added ajaxSuccess and removed id from index.html

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -8,26 +8,18 @@ $.ajax({
     for(var i=0; i< data.length; i++){
 
         console.log(data[i]._id + ': ' + data[i].title + ': ' + data[i].text);
-        $('#showThreads').append('<p>' + '<b>id: </b>' + '<p id="thread' + data[i]._id + '" class="linkThread">' + data[i]._id + '</p>' + ' <b>title:</b> ' + data[i].title + '<br />' +' <b>text:</b> ' + data[i].text + '</p><hr /><br />');
+        $('#showThreads').append('<p>' + ' <b>title:</b> <p class="linkThread" id="'+ data[i]._id +'">' + data[i].title + '<br /><p>' +' <b>text:</b> ' + data[i].text + '</p><hr /><br />');
 
     }
 });
 
 
-$('#submitButton').on('click', function() {
+$('#btnCreateNewThread').on('click', function() {
 
     var $title = $('#title2').val();
-    var $id = $.ajax({
-        url: "/threads"
-    }).done(function (data) {
-        console.log('Fetched / threads: ', data);
-        for (var i = data.length; i > 0; i -= data.length) {
-            $id = data[data.length - 1]._id;
-            console.log($id);
-            return $id;
-        }
-    });
+
     var $text = $('#text2').val();
+
     $.ajax({
             method: "POST",
             url: "/threads",
@@ -36,7 +28,26 @@ $('#submitButton').on('click', function() {
         .done(function( ) {
 
             console.log( "Data Saved: " + 'title: ' + $title + ' : ' + 'text: ' +$text);
-            $('#showThreads').append('<p>' + ' <b>id:</b> ' + '<p class="linkThread" id="thread' + $id + '">' + $id + '</p>' + '<br />' + ' <b>title:</b> ' + $title + '<br />' +' <b>text:</b> ' + $text + '</p><hr /><br />');
+            $('#showThreads').append('<p>' + ' <b>title: </b><p class="linkThread">' + $title + '</p><br />' +' <b>text:</b> ' + $text + '</p><hr /><br />');
         });
 
 });
+
+
+$( document ).ajaxSuccess(function( event, request, settings ) {
+
+    //Kod för att hämta ut id på vem som gjorde anropet
+    var eventTracker = event.target.activeElement;
+    console.log(eventTracker);
+    console.log(eventTracker.id);
+
+    //Kod för att få ut ett object med data på trådar.
+    // Kan hämta ut id:t till senaste postade tråden
+    console.log(request.responseText);
+var serverResponseObject = JSON.parse(request.responseText);
+    console.log(serverResponseObject);
+    console.log(serverResponseObject._id);
+
+});
+
+
