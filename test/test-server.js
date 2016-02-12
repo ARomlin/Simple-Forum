3542e939f94f28e68474a05177653de1acda8bcc
graneman <graneman@aglaptop.localdomain>
2016-02-12 10:58:46 +0100

    Added test for creating a thread

--- test/test-server.js
+++ test/test-server.js
@@ -15,7 +15,18 @@ it('should list ALL data objects on /threads GET', function(done) {
     });
 });
   it('should list a SINGLE object on /threads/<id> GET');
-  it('should add a SINGLE object on /threads POST');
+  it('should add a SINGLE object on /threads POST', function(done) {
+  // Add a timestamp to the text
+  var myDateTimeStamp = new Date();
+   
+  chai.request(server)
+  .post('/threads')
+  .send({ title: 'myTitle '+ myDateTimeStamp, text: 'myText ' + myDateTimeStamp})
+    .end(function(err, res){
+      res.should.have.status(201);
+      done();
+    });
+  });  
   it('should update a SINGLE object on /threads/<id> PUT');
   it('should delete a SINGLE object on /threads/<id> DELETE');
 });
